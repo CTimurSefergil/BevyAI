5ad62456ccd3a421ed264e402bd14e7ef906e2af
Timur Sefergil <140344749+CTimurSefergil@users.noreply.github.com>
2024-06-11 12:03:33 +0300

    Addet not working transparency function to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use bevy::prelude::*;
+use image::*;
 use openai_api_rust::images::*;
 use openai_api_rust::*;
 use std::{fs::File, io::copy};
@@ -21,9 +22,9 @@ fn image_create() {
     let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
 
     let body = ImagesBody {
-        prompt: "A cartoonish style skyscraper".to_string(),
+        prompt: "Draw a cartoonish style 16 bit female wizard".to_string(),
         n: Some(1),
-        size: Some("512x512".to_string()),
+        size: Some("256x256".to_string()),
         response_format: None,
         user: None,
     };
@@ -31,14 +32,31 @@ fn image_create() {
     let images = rs.unwrap().data.unwrap();
     let image = images.get(0).unwrap();
     if image.url.contains("http") {
-        let _ = download_image(&image.url as &str, r#"assets/image.png"#);
+        let _ = download_and_save_image(&image.url as &str, r#"assets/image.png"#);
+        convert_to_transparent(r#"assets/image.png"#);
     }
 }
 
-fn download_image(url: &str, file_name: &str) -> Result<()> {
+fn download_and_save_image(url: &str, file_name: &str) -> Result<()> {
     let mut response = reqwest::blocking::get(url)?;
     let mut file = File::create(file_name)?;
     copy(&mut response, &mut file)?;
 
     Ok(())
+}
+
+// NEDEN ÇALIŞMIYORSUN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+fn convert_to_transparent(path: &str) {
+    let img = image::open(path).unwrap();
+    let mut file = File::create(format!(r#"assets/image1.png"#)).unwrap();
+    copy(&mut img.as_bytes(), &mut file).unwrap();
+
+    let mut img = img.to_rgba32f();
+    for p in img.pixels_mut() {
+        if p[0] == 255.0 && p[1] == 255.0 && p[2] == 255.0 {
+            p[3] = 0.0;
+        }
+    }
+
+    std::io::copy(&mut img.as_bytes(), &mut file).unwrap();
 }
