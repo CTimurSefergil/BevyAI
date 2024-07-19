05e305f4110ecb8dc0ff736b68c6749751938bef
Timur Sefergil <140344749+CTimurSefergil@users.noreply.github.com>
2024-07-19 12:59:52 +0300

    Update main.rs
    Added a character and movement.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
+    tasks::{AsyncComputeTaskPool, Task},
     utils::hashbrown::HashSet,
     window::close_on_esc,
 };
@@ -11,20 +12,27 @@ use noise::{NoiseFn, Perlin, Seedable};
 use openai_api_rust::images::*;
 use openai_api_rust::*;
 use rand::Rng;
-use std::{fs::File, io::copy};
+use std::{fs::File, io::copy, process::Command};
 
-const GRASS_PATH: &str = "grass1.png";
-const WATER_PATH: &str = "water1.png";
+const GRASS_PATH: &str = "grass_transparent.png";
+const WATER_PATH: &str = "water_transparent.png";
 const SPRITE_SCALE_FACTOR: usize = 1;
 
-const GRID_COLS: usize = 200;
-const GRID_ROWS: usize = 100;
+const GRID_COLS: usize = 500;
+const GRID_ROWS: usize = 250;
 const BG_COLOR: (u8, u8, u8) = (0, 187, 163);
 
 const TILE_W: usize = 256;
 const TILE_H: usize = 256;
 
-const NOISE_SCALE: f64 = 0.07;
+const NOISE_SCALE: f64 = 0.01;
+
+const MOVEMENT_SPEED: f32 = 840.0;
+const CAM_LERP_FACTOR: f32 = 2.;
+
+#[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Default, Reflect)]
+#[reflect(Component)]
+pub struct Player;
 
 fn main() {
     App::new()
@@ -36,14 +44,25 @@ fn main() {
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_plugins(LogDiagnosticsPlugin::default())
         .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (handle_player_movement_input, update_camera).chain(),
+        )
         .add_systems(Update, close_on_esc)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    create_image("Create an izometric grass texture", "grass");
-    create_image("Create an izometric water texture", "water");
-
+    /*create_image(
+        "Create a 16 bit female witch character with white blackground",
+        "grass",
+    );
+    create_image("Create a cartoonish water texture", "water");
+    create_image(
+        "Create a single female warrior character with white blackground",
+        "character",
+    );
+    */
     commands
         .spawn(Camera2dBundle::default())
         .insert(PanCam::default());
@@ -92,6 +111,21 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..default()
         });
     }
+
+    commands.spawn((
+        Name::new("Player"),
+        Player,
+        SpriteSheetBundle {
+            sprite: Sprite::default(),
+            transform: Transform::from_scale(Vec3::splat(16.0 as f32)).with_translation(Vec3 {
+                x: (100.0),
+                y: (100.0),
+                z: (0.0),
+            }),
+            texture: asset_server.load("character_transparent.png"),
+            ..default()
+        },
+    ));
 }
 
 fn grid_to_world(x: f32, y: f32) -> (f32, f32) {
@@ -136,7 +170,7 @@ fn download_and_save_image(url: &str, file_name: String) -> Result<()> {
 }
 
 fn convert_to_transparent(path: &str, path1: &str) {
-    let save_path = path1.to_owned() + "1" + ".png";
+    let save_path = path1.to_owned() + "_transparent" + ".png";
     let image = image::open(path).unwrap();
     let mut rgbaimage = image.to_rgba8();
     let (width, height) = image.dimensions();
@@ -155,6 +189,57 @@ fn convert_to_transparent(path: &str, path1: &str) {
         }
     }
     rgbaimage.save(save_path).unwrap();
+}
+
+fn handle_player_movement_input(
+    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<(&mut Transform, &mut Sprite), With<Player>>,
+) {
+    let mut intent = Vec3::ZERO;
+    if input.pressed(KeyCode::KeyW) || input.pressed(KeyCode::ArrowUp) {
+        intent.y += 1.0;
+    }
+    if input.pressed(KeyCode::KeyS) || input.pressed(KeyCode::ArrowDown) {
+        intent.y -= 1.0;
+    }
+    if input.pressed(KeyCode::KeyA) || input.pressed(KeyCode::ArrowLeft) {
+        intent.x -= 1.0;
+    }
+    if input.pressed(KeyCode::KeyD) || input.pressed(KeyCode::ArrowRight) {
+        intent.x += 1.0;
+    }
+
+    let intent = intent.normalize_or_zero();
+    let target_velocity = intent * MOVEMENT_SPEED;
+
+    for (mut transform, mut sprite) in &mut player_query {
+        transform.translation += target_velocity * time.delta_seconds();
+        if intent.x != 0.0 {
+            sprite.flip_x = intent.x < 0.0;
+        }
+    }
+}
+
+fn update_camera(
+    mut camera: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player: Query<&Transform, (With<Player>, Without<Camera2d>)>,
+    time: Res<Time>,
+) {
+    let Ok(mut camera) = camera.get_single_mut() else {
+        return;
+    };
+
+    let Ok(player) = player.get_single() else {
+        return;
+    };
+
+    let Vec3 { x, y, .. } = player.translation;
+    let direction = Vec3::new(x, y, camera.translation.z);
+
+    camera.translation = camera
+        .translation
+        .lerp(direction, time.delta_seconds() * CAM_LERP_FACTOR);
 }
 
 #[cfg(test)]
